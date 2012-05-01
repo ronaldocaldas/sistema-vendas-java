7f827ef96216e743b43e3d4c9f46d072bd06a38f
ronaldo_caldaz <ronaldo_caldaz@yahoo.com.br>
2012-05-01 19:17:57 +0000

    Criação das configurações do hibernate.

---
+++ sistema-vendas-java/JavaSource/com/kurtphpr/sistema/vendas/HibernateUtil.java
@@ -0,0 +1,25 @@
+package com.kurtphpr.sistema.vendas;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+	
+	private static final SessionFactory session = buildSessionFactory();
+
+	private static SessionFactory buildSessionFactory() {
+		try {
+			Configuration cfg =  new Configuration();
+			cfg.configure("hibernate.cfg.xml");
+			return cfg.buildSessionFactory();
+		} catch (Throwable e) {
+			System.out.println("Deu pau! Manolo!\n " + e );
+			throw new ExceptionInInitializerError();
+		}
+	}
+
+	public static SessionFactory getSession() {
+		return session;
+	}
+
+}
