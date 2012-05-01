9bbe66c472da12d7abc1af62ecbccd0417491f6f
ronaldo_caldaz@yahoo.com.br <ronaldo_caldaz@yahoo.com.br@676d58bb-0a5b-0709-23cc-5092edaabadc>
2012-05-01 19:17:57 +0000

    Criação das configurações do hibernate.

---
+++ trunk/sistema-vendas-java/JavaSource/Conecta.java
@@ -0,0 +1,19 @@
+import org.hibernate.Session;
+
+import com.kurtphpr.sistema.vendas.HibernateUtil;
+
+
+public class Conecta {
+
+	public static void main(String[] args) {
+		Session  sessao = null;
+		try {
+			sessao = HibernateUtil.getSession().openSession();
+			System.out.println("Conectou !");
+		}finally{
+			sessao.close();
+			System.out.println("Fechou conexão!");
+		}
+		
+	}
+}
