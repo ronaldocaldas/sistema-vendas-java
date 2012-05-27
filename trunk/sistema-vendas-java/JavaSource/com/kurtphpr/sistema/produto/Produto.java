3a14d2c98990860b1c81a660e831ce94bcc42f4b
ronaldo_caldaz@yahoo.com.br <ronaldo_caldaz@yahoo.com.br@676d58bb-0a5b-0709-23cc-5092edaabadc>
2012-05-27 23:15:34 +0000

    Aula 5: mapeamento da classe Produto e classe Venda.

---
+++ trunk/sistema-vendas-java/JavaSource/com/kurtphpr/sistema/produto/Produto.java
@@ -0,0 +1,78 @@
+package com.kurtphpr.sistema.produto;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "produto")
+public class Produto {
+
+	@Id
+	@GeneratedValue
+	private Integer id;
+
+	private String unidade;
+
+	private String descricao;
+
+	@Column(name = "data_cadastro")
+	private Date dataCadastro;
+
+	private Integer estoque;
+
+	private float valor;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getUnidade() {
+		return unidade;
+	}
+
+	public void setUnidade(String unidade) {
+		this.unidade = unidade;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Date getDataCadastro() {
+		return dataCadastro;
+	}
+
+	public void setDataCadastro(Date dataCadastro) {
+		this.dataCadastro = dataCadastro;
+	}
+
+	public Integer getEstoque() {
+		return estoque;
+	}
+
+	public void setEstoque(Integer estoque) {
+		this.estoque = estoque;
+	}
+
+	public float getValor() {
+		return valor;
+	}
+
+	public void setValor(float valor) {
+		this.valor = valor;
+	}
+
+}
