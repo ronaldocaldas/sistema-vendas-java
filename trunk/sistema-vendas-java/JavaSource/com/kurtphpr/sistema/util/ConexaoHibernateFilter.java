46cc6b83f1aebded5187872a077648c309ada2eb
ronaldo_caldaz@yahoo.com.br <ronaldo_caldaz@yahoo.com.br@676d58bb-0a5b-0709-23cc-5092edaabadc>
2012-06-15 03:30:55 +0000

    Aula 6: Criação e mapeamento de filtro para sessão.

---
+++ trunk/sistema-vendas-java/JavaSource/com/kurtphpr/sistema/util/ConexaoHibernateFilter.java
@@ -0,0 +1,53 @@
+package com.kurtphpr.sistema.util;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+import org.hibernate.SessionFactory;
+
+import com.kurtphpr.sistema.vendas.HibernateUtil;
+
+public class ConexaoHibernateFilter implements Filter{
+	
+	private SessionFactory sf;
+
+	@Override
+	public void destroy() {
+	}
+
+	@Override
+	public void doFilter(ServletRequest servletFilter, ServletResponse servletResponse,
+			FilterChain chain) throws IOException, ServletException {
+		
+		try {
+			this.sf.getCurrentSession().beginTransaction();
+			chain.doFilter(servletFilter, servletResponse);
+			this.sf.getCurrentSession().getTransaction().commit();
+			this.sf.getCurrentSession().close();
+		} catch (Throwable ex) {
+			
+			try {
+				if(this.sf.getCurrentSession().getTransaction().isActive()){
+					this.sf.getCurrentSession().getTransaction().rollback();
+				}
+			} catch (Throwable t) {
+				t.printStackTrace();
+			}
+			
+			throw new ServletException();
+		}
+	
+	}
+
+	@Override
+	public void init(FilterConfig conf) throws ServletException {
+		this.sf = HibernateUtil.getSession();
+	}
+
+}
