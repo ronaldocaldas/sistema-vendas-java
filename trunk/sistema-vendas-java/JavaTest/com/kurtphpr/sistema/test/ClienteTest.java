67147436746f2775f9b4a8cb0dcffee73d70d44e
ronaldo_caldaz@yahoo.com.br <ronaldo_caldaz@yahoo.com.br@676d58bb-0a5b-0709-23cc-5092edaabadc>
2013-07-14 20:28:40 +0000

    Aula 18 Eclipse e JSF 2.0 Vendas: Método de exclusão de clientes com TDD.

--- trunk/sistema-vendas-java/JavaTest/com/kurtphpr/sistema/test/ClienteTest.java
+++ trunk/sistema-vendas-java/JavaTest/com/kurtphpr/sistema/test/ClienteTest.java
@@ -2,11 +2,14 @@ package com.kurtphpr.sistema.test;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.Date;
 import java.util.List;
 
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.junit.After;
 import org.junit.AfterClass;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -46,6 +49,31 @@ public class ClienteTest {
 
 		}
 	}
+	
+	@Before
+	public void setup(){
+		Cliente c1 = new Cliente("34125365474", "teste1@mail", "Rua 1", "Cliente 1", new Date(), 2000);
+		Cliente c2 = new Cliente("34125365474", "teste2@mail", "Rua 2", "Cliente 2", new Date(), 3000);
+		Cliente c3 = new Cliente("34125365474", "teste3@mail", "Rua 3", "Cliente 3", new Date(), 4000);
+		
+		ClienteRN clienteRN = new ClienteRN();
+		
+		clienteRN.salvar(c1);
+		clienteRN.salvar(c2);
+		clienteRN.salvar(c3);
+		
+	}
+	
+	@After
+	public void limpaBanco(){
+		
+		ClienteRN clienteRN = new ClienteRN();
+		List<Cliente> lista = clienteRN .listar();
+		
+		for (Cliente cliente : lista) {
+			clienteRN.excluir(cliente);
+		}
+	}
 
 	@Test
 	public void salvarTest() {
@@ -69,7 +97,25 @@ public class ClienteTest {
 	public void listarTest(){
 		ClienteRN clienteRN = new ClienteRN();
 		 List<Cliente> lista = clienteRN.listar();
-		 assertEquals(5, lista.size());
+		 assertEquals(3, lista.size());
 		 
 	}
+	
+	@Test
+	public void excluirTest(){
+		ClienteRN clienteRN = new ClienteRN();
+		
+		List<Cliente> lista = clienteRN.listar();
+		
+		Cliente clienteExcluido = lista.get(0);
+		
+		clienteRN.excluir(clienteExcluido);
+		
+		lista = clienteRN.listar();
+		
+		assertEquals(2, lista.size());
+		
+	}
+	
+	
 }
