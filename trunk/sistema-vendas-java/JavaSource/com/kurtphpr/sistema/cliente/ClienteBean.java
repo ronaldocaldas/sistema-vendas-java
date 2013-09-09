902373a4d615f66933471cf6ef5a47f88545e0e1
ronaldo_caldaz@yahoo.com.br <ronaldo_caldaz@yahoo.com.br@676d58bb-0a5b-0709-23cc-5092edaabadc>
2013-09-09 02:12:27 +0000

    Aula 21 Eclipse e JSF 2.0 Vendas: implementando o método de edição de cliente.

--- trunk/sistema-vendas-java/JavaSource/com/kurtphpr/sistema/cliente/ClienteBean.java
+++ trunk/sistema-vendas-java/JavaSource/com/kurtphpr/sistema/cliente/ClienteBean.java
@@ -8,24 +8,28 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import javax.faces.context.FacesContext;
 
-@ManagedBean(name="clienteBean")
+@ManagedBean(name = "clienteBean")
 @RequestScoped
 public class ClienteBean {
-	
-	private Cliente clienteSelecionado =  new Cliente();
-	
+
+	private Cliente clienteSelecionado = new Cliente();
+
 	private List<Cliente> lista = null;
-	
-	public void salvar(){
+
+	public void salvar() {
 		ClienteRN clienteRN = new ClienteRN();
 		clienteSelecionado.setDataCadastro(new Date());
-		clienteRN.salvar(clienteSelecionado);
-		FacesMessage faces = new FacesMessage("Cliente cadastrado com sucesso!");
-		FacesContext contexto = FacesContext.getCurrentInstance();
-		contexto.addMessage(null, faces);
-		
+		if (this.clienteSelecionado.getId() != null	&& this.clienteSelecionado.getId() != 0) {
+			clienteRN.alterar(this.clienteSelecionado);
+		} else {
+			clienteRN.salvar(clienteSelecionado);
+			FacesMessage faces = new FacesMessage(
+					"Cliente cadastrado com sucesso!");
+			FacesContext contexto = FacesContext.getCurrentInstance();
+			contexto.addMessage(null, faces);
+		}
 		this.lista = null;
-		
+
 	}
 
 	public Cliente getClienteSelecionado() {
@@ -38,15 +42,19 @@ public class ClienteBean {
 
 	public List<Cliente> getLista() {
 		ClienteRN clienteRN = new ClienteRN();
-		if(lista == null){
+		if (lista == null) {
 			lista = clienteRN.listar();
 		}
 		return lista;
 	}
 
-	public void excluir(){
+	public void excluir() {
 		ClienteRN clienteRN = new ClienteRN();
 		clienteRN.excluir(this.clienteSelecionado);
 		this.lista = null;
+	}
+
+	public void novo() {
+		this.clienteSelecionado = new Cliente();
 	}
 }
