6126c5cab94862017d1b7061ccac8f78668de609
ronaldo_caldaz@yahoo.com.br <ronaldo_caldaz@yahoo.com.br@676d58bb-0a5b-0709-23cc-5092edaabadc>
2013-12-07 23:27:07 +0000

    Aula 25 Eclipse e JSF 2.0 Vendas: implementando a pagina do registro de vendas.

---
+++ trunk/sistema-vendas-java/JavaSource/com/kurtphpr/sistema/venda/RegistroVendaBean.java
@@ -0,0 +1,87 @@
+package com.kurtphpr.sistema.venda;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+import com.kurtphpr.sistema.cliente.Cliente;
+import com.kurtphpr.sistema.produto.Produto;
+import com.kurtphpr.sistema.produto.ProdutoRN;
+
+@ManagedBean(name=  "registroVendas")
+@ViewScoped
+public class RegistroVendaBean {
+	
+	private Cliente clienteSelecionado;
+	private Produto produtoSelecionado = new Produto() ;
+	private List<Produto> carrinhoCompras = new ArrayList<Produto>();
+	
+	private float valorTotal;
+	
+	
+	public void getBuscaProduto(){
+		
+		
+		ProdutoRN produtoRN = new ProdutoRN();
+		Produto produtoPesquisado = new Produto();
+		
+		if(this.produtoSelecionado.getDescricao() != null && !this.produtoSelecionado.getDescricao().equals("")){
+			produtoPesquisado = produtoRN.pesquisarPorNome(this.produtoSelecionado.getDescricao());
+			
+			if(produtoPesquisado != null){
+				
+				this.carrinhoCompras.add(produtoPesquisado);
+				calculaTotal();
+				
+			}
+			
+		}
+		
+	}
+
+	private void calculaTotal() {
+	
+		if (!this.carrinhoCompras.isEmpty()) {
+			for (Produto p : this.carrinhoCompras) {
+				valorTotal =+ p.getValor();
+			}
+		}
+		
+		
+	}
+
+	public Cliente getClienteSelecionado() {
+		return clienteSelecionado;
+	}
+
+	public void setClienteSelecionado(Cliente clienteSelecionado) {
+		this.clienteSelecionado = clienteSelecionado;
+	}
+
+	public Produto getProdutoSelecionado() {
+		return produtoSelecionado;
+	}
+
+	public void setProdutoSelecionado(Produto produtoSelecionado) {
+		this.produtoSelecionado = produtoSelecionado;
+	}
+
+	public List<Produto> getCarrinhoCompras() {
+		return carrinhoCompras;
+	}
+
+	public void setCarrinhoCompras(List<Produto> carrinhoCompras) {
+		this.carrinhoCompras = carrinhoCompras;
+	}
+
+	public float getValorTotal() {
+		return valorTotal;
+	}
+
+	public void setValorTotal(float valorTotal) {
+		this.valorTotal = valorTotal;
+	}
+	
+}
